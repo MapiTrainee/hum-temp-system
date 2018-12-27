e82d8cb821f3e6e3fbaaee0d48c0386557faf135
Mark <mpietryga93@gmail.com>
2018-12-27 13:11:26 +0100

    Create project structure with Spring Initializer

---
+++ src/main/java/eu/mapidev/pi/htsystem/HumTempSystemApplication.java
@@ -0,0 +1,14 @@
+package eu.mapidev.pi.htsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumTempSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumTempSystemApplication.class, args);
+	}
+
+}
+
